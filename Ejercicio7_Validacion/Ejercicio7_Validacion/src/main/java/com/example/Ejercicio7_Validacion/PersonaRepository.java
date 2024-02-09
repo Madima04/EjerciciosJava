8196653112283bf71a280eabcd08ff8da43b7a61
Mario Díaz <125073398+Madima04@users.noreply.github.com>
2024-02-09 09:42:16 +0100

    Borrado rama

--- Ejercicio7_Validacion/Ejercicio7_Validacion/src/main/java/com/example/Ejercicio7_Validacion/PersonaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Ejercicio7_Validacion;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface PersonaRepository extends JpaRepository<Persona, Integer> {
-}
