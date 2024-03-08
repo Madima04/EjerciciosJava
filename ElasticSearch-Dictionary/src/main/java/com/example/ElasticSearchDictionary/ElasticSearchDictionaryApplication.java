f6b1adb2110ca299a461dac815796455adc4ebbb
Mario Díaz <125073398+Madima04@users.noreply.github.com>
2024-03-08 12:04:43 +0100

    Empezando proyecto ElasticSearch

---
+++ ElasticSearch-Dictionary/src/main/java/com/example/ElasticSearchDictionary/ElasticSearchDictionaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.ElasticSearchDictionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticSearchDictionaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticSearchDictionaryApplication.class, args);
+	}
+
+}
