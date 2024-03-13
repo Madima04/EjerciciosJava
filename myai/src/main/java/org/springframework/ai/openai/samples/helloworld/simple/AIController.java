c3b57e3dba741048a1c25a666d77e4033e50d3c0
Mario Díaz <125073398+Madima04@users.noreply.github.com>
2024-03-13 13:40:56 +0100

    Spring Boot IA con memoria

---
+++ myai/src/main/java/org/springframework/ai/openai/samples/helloworld/simple/AIController.java
@@ -0,0 +1,32 @@
+package org.springframework.ai.openai.samples.helloworld.simple;
+
+import dev.langchain4j.memory.ChatMemory;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+
+import dev.langchain4j.service.AiServices;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import org.springframework.ai.openai.samples.helloworld.config.ApiKeys;
+import org.springframework.ai.openai.samples.helloworld.config.Assistant;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RequestMapping("/ai")
+@RestController
+public class AIController {
+	ChatMemory chatMemory = MessageWindowChatMemory.withMaxMessages(10);
+
+	Assistant assistant = AiServices.builder(Assistant.class)
+			.chatLanguageModel(OpenAiChatModel.withApiKey(ApiKeys.OPENAI_API_KEY))
+			.chatMemory(chatMemory)
+			.build();
+
+	@PostMapping("/message")
+	public String chat(@RequestParam String message) {
+		return   assistant.chat(message);
+	}
+
+}
+
