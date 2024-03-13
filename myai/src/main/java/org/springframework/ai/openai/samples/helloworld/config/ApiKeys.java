96ad4dc6572e37aec140d1d83aacd1cff851beb2
Mario Díaz <125073398+Madima04@users.noreply.github.com>
2024-03-13 13:42:43 +0100

    Contraseña oculta

---
+++ myai/src/main/java/org/springframework/ai/openai/samples/helloworld/config/ApiKeys.java
@@ -0,0 +1,5 @@
+package org.springframework.ai.openai.samples.helloworld.config;
+
+public class ApiKeys {
+    public static final String OPENAI_API_KEY = "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";
+}
